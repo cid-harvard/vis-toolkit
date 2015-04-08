c3a3d38b069c6a6d28e1afd43437b2127ea6c9a1
romsson <romain.vuillemot@gmail.com>
2015-04-08 16:40:56 -0400

    Starting pie chart visualization

--- js/vistk.js
+++ js/vistk.js
@@ -660,6 +660,7 @@ vistk.viz = function() {
                             return "translate("+d.rank+", "+vars.height/2+")";
                           })
 
+
         } else {
 
           vars.svg.selectAll(".points")
@@ -669,7 +670,37 @@ vistk.viz = function() {
                           })
         }
 
+      } else if(vars.type == "piechart") {
 
+        var r = vars.width/6;
+        
+        var color = d3.scale.category20c();
+
+        vis = d3.select(vars.container)
+                    .data(new_data)
+                  .enter()
+                    .append("g")
+                    .attr("transform", "translate(" + vars.width/2 + "," + vars.height/2 + ")");
+        
+        var pie = d3.layout.pie().value(function(d){ return 1; }); // equal share
+
+        // declare an arc generator function
+        var arc = d3.svg.arc().outerRadius(r);
+
+        // select paths, use arc generator to draw
+        var arcs = vis.selectAll("g.slice")
+                      .data(pie)
+                    .enter()
+                      .append("svg:g")
+                      .attr("class", "slice");
+        
+        arcs.append("svg:path")
+            .attr("fill", function(d, i){
+              return color(i);
+            })
+            .attr("d", function (d) {
+              return arc(d);
+            });
 
       } else if(vars.type == "nodelink") {
 
@@ -1051,6 +1082,12 @@ vistk.viz = function() {
   chart.color = function(x) {
     if (!arguments.length) return vars.var_color;
     vars.var_color = x;
+    return chart;
+  };
+
+  chart.share = function(x) {
+    if (!arguments.length) return vars.var_share;
+    vars.var_share = x;
     return chart;
   };
 
