35c9e7a94c9cf71b3e75dd21971bf776ca54886e
romsson <romain.vuillemot@gmail.com>
2015-05-15 12:47:21 -0400

    Changing scales

--- src/visualizations/sparkline.js
+++ src/visualizations/sparkline.js
@@ -6,22 +6,21 @@
         // Or do something to build the temporal data? Should it happen here?
         vars.data.forEach(function(d) {
 
-          if(d.dept_name == "Antioquia")
+          if(d.dept_name === "Antioquia")
             new_data.push({name: d.dept_name, year: vars.time.parse(d.year), realgdp: d.realgdp})
 
-        })
+        });
 
-        // From http://www.tnoda.com/blog/2013-12-19
-        var x = d3.scale.linear().range([0, vars.width - 2]);
-        var y = d3.scale.linear().range([vars.height - 4, 0]);
+        vars.x_scale = d3.scale.linear().range([0, vars.width - 2]);
+        vars.y_scale = d3.scale.linear().range([vars.height - 4, 0]);
 
         var line = d3.svg.line()
                      .interpolate("basis")
-                     .x(function(d) { return x(d[vars.var_time]); })
-                     .y(function(d) { return y(d[vars.var_y]); });
+                     .x(function(d) { return vars.x_scale(d[vars.var_time]); })
+                     .y(function(d) { return vars.y_scale(d[vars.var_y]); });
         
-        x.domain(d3.extent(new_data, function(d) { return d[vars.var_time]; }));
-        y.domain(d3.extent(new_data, function(d) { return d[vars.var_y]; }));
+        vars.x_scale.domain(d3.extent(new_data, function(d) { return d[vars.var_time]; }));
+        vars.y_scale.domain(d3.extent(new_data, function(d) { return d[vars.var_y]; }));
 
         vars.svg.selectAll(".sparkline").data([new_data])
           .enter().append('path')
@@ -31,14 +30,14 @@
 
         vars.svg.append('circle')
            .attr('class', 'start sparkcircle')
-           .attr('cx', x(new_data[0][vars.var_time]))
-           .attr('cy', y(new_data[0][vars.var_y]))
+           .attr('cx', vars.x_scale(new_data[0][vars.var_time]))
+           .attr('cy', vars.y_scale(new_data[0][vars.var_y]))
            .attr('r', 1.5);  
 
         vars.svg.append('circle')
            .attr('class', 'end sparkcircle')
-           .attr('cx', x(new_data[new_data.length-1][vars.var_time]))
-           .attr('cy', y(new_data[new_data.length-1][vars.var_y]))
+           .attr('cx', vars.x_scale(new_data[new_data.length-1][vars.var_time]))
+           .attr('cy', vars.y_scale(new_data[new_data.length-1][vars.var_y]))
            .attr('r', 1.5);  
 
         break;
