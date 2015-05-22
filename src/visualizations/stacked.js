8b0ddf5a924a0c19c7e33c0972527850cac162eb
romsson <romain.vuillemot@gmail.com>
2015-05-22 14:51:09 -0400

    Stacked integration

--- src/visualizations/stacked.js
+++ src/visualizations/stacked.js
@@ -6,6 +6,101 @@
         vars.evt.register("selection", function(d) { });
         vars.evt.register("resize", function(d) { });
 
+        var parseDate = d3.time.format("%y-%b-%d").parse,
+            formatPercent = d3.format(".0%");
+
+        var x = d3.time.scale()
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var color = d3.scale.category20();
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(formatPercent);
+
+        var area = d3.svg.area()
+            .interpolate('cardinal')
+            .x(function(d) { return x(d.date); })
+            .y0(function(d) { return y(d.y0); })
+            .y1(function(d) { return y(d.y0 + d.y); });
+
+        var stack = d3.layout.stack()
+            .values(function(d) { return d.values; });
+
+        var parseDate = d3.time.format("%Y").parse;
+
+        // Find the number or years
+
+        unique_years = d3.set(vars.data.data.map(function(d) { return d.year;})).values();
+
+        data = [];
+
+        unique_years.forEach(function(d) {
+
+          a = {};
+
+          vars.data.data.filter(function(e) {
+            return e.year == d;
+          })
+          .map(function(e) {
+            a[e.abbrv] = e.share;
+      //      return a;
+          })
+
+          a.date = parseDate(d);
+          data = data.concat(a);
+
+        })
+
+        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+        browsers = stack(color.domain().map(function(name) {
+          return {
+            name: name,
+            values: data.map(function(d) {
+              return {date: d.date, y: d[name]};
+            })
+          };
+        }));
+
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+
+        var browser = vars.svg.selectAll(".browser")
+            .data(browsers)
+          .enter().append("g")
+            .attr("class", "browser");
+
+        browser.append("path")
+            .attr("class", "area")
+            .attr("d", function(d) { return area(d.values); })
+            .style("fill", function(d) { return color(d.name); });
+
+        browser.append("text")
+            .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+            .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+            .attr("x", -6)
+            .attr("dy", ".35em")
+            .text(function(d) { return d.name; });
+
+        vars.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        vars.svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+/*
+
         // PRE-UPDATE
         var gPoints = vars.svg.selectAll(".mark__group")
                          .data(vars.new_data, function(d, i) { return i; });
@@ -36,5 +131,5 @@
         // POST-UPDATE
         gPoints
             .transition();
-
+*/
       break;
