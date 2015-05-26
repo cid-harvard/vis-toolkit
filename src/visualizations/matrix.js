e9903b72f3c5dd28df1ac32132de4be5fede6beb
romsson <romain.vuillemot@gmail.com>
2015-05-26 11:50:59 -0400

    Init matrix visualization

---
+++ src/visualizations/matrix.js
@@ -0,0 +1,40 @@
+      case "matrix":
+
+        // REGISTER EVENTS
+        vars.evt.register("highlightOn", function(d) { });
+        vars.evt.register("highlightOut", function(d) { });
+        vars.evt.register("selection", function(d) { });
+        vars.evt.register("resize", function(d) { });
+
+        // PRE-UPDATE
+        var gItems = vars.svg.selectAll(".mark__group")
+                         .data(vars.new_data, function(d, i) { return i; });
+
+        // ENTER
+
+        // Add a group for marks
+        var gItems_enter = gItems.enter()
+                        .append("g")
+                        .each(vistk.utils.items_group);
+
+        // Add a graphical mark
+        gItems_enter.each(vistk.utils.items_mark);
+
+        // Add an other graphical mark (e.g. text labels)
+
+        // Add a connection mark
+        gItems_enter.each(vistk.utils.connect_mark)
+
+        // Add axis
+        gItems_enter.each(vistk.utils.axis)
+
+        // Add grid layer
+
+        // EXIT
+        var gItems_exit = gItems.exit();
+
+        // POST-UPDATE
+        gItems
+            .transition();
+
+      break;
