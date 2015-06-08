063ed202d90f80597108fa132afd7e1567aa8c22
romsson <romain.vuillemot@gmail.com>
2015-06-08 13:34:21 -0400

    Cleaning up getter setters

--- src/getterssetters.js
+++ src/getterssetters.js
@@ -1,46 +1,8 @@
 
-  // If init, then dispatch those events
-  // TODO: dispatch focus event here and highlight nodes
-  if(vars.selection.length > 0) {
-
-    var selection_points = gItems
-      .filter(function(d, i) {
-        return i === vars.selection[0];
-      });
-
-    selection_points.select(".dot__circle")
-      .classed("selected", false);
-
-    selection_points.select(".dot__label")
-      .filter(function(d, i) {
-        return i === vars.selection[0];
-      })
-      .classed("selected", true);
-
-  }
-
-  if(typeof vars.highlight !== undefined && vars.highlight.length > 0) {
-
-    vars.evt.call("highlightOn", vars.new_data[vars.highlight]);
-  
-  }
-
-  // Public Variables
-  chart.id = function(x) {
-    if (!arguments.length) return vars.var_id;
-    vars.var_id = x;
-    return chart;
-  };
-
-	chart.height = function(x) {
-	  if (!arguments.length) return vars.height;
-	  vars.height = x;
-	  return chart;
-	};
-
-  chart.width = function(x) {
-    if (!arguments.length) return vars.width;
-    vars.width = x;
+  chart.params = function(x) {
+    if (!arguments.length) return vars;
+    vars = vistk.utils.merge(vars, x);
+    vars.user_config = x;    
     return chart;
   };
 
@@ -50,23 +12,6 @@
     return chart;
   };
 
-  chart.title = function(x) {
-    if (!arguments.length) return vars.title;
-    vars.title = x;
-    return chart;
-  };
-
-  chart.type = function(x) {
-    if (!arguments.length) return vars.type;
-    vars.type = x;
-    return chart;
-  };
-
-  chart.data = function(x) {
-    if (!arguments.length) return vars.data;
-    vars.data = x;
-    return chart;
-  };
 
   chart.join = function(data, var_join) {
 
@@ -90,14 +35,6 @@
       });
     });
 
-    return chart;
-  };
-
-  // Generic parameters function
-  chart.params = function(x) {
-    if (!arguments.length) return vars;
-    vars = vistk.utils.merge(vars, x);
-    vars.user_config = x;    
     return chart;
   };
 
