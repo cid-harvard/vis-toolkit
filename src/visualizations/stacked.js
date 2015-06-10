7baf8f5ab159ca5e3d12c962fff6d21c1f993bbb
romsson <romain.vuillemot@gmail.com>
2015-06-10 17:19:40 -0400

    Progress on stacked parameters

--- src/visualizations/stacked.js
+++ src/visualizations/stacked.js
@@ -3,37 +3,55 @@
         vars.params = {
 
           accessor_values: function(d) { return d.values; },
+          accessor_items: function(d) { return d; },
 
           x_scale: [{
               name: "linear",
               func: d3.time.scale()
-                  .range([0, vars.width-100])
-                  .domain(d3.extent(vars.data, function(d) { return d[vars.var_time]; }))
+                      .range([0, vars.width])
             }
           ],
 
           y_scale: [{
               name: "linear",
               func: d3.scale.linear()
-                      .range([vars.height - 4, 0])
-                      .domain(d3.extent(vars.data, function(d) { return d[vars.var_y]; }))
+                      .range([0, vars.height])
+                      .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_y]; }))
+
           }],
 
-          items: [],
+          items: [{
+            attr: "year",
+            marks: [{
+                type: "circle",
+                rotate: "0",
+                fill: function(d) { return vars.color(vars.accessor_items(d)[vars.var_color]); }
+              }, {
+                type: "text",
+                rotate: "0",
+                translate: null
+              }]
+          }],
 
           connect: [{
             attr: vars.time.var_time,
             marks: [{
                 type: "path",
                 rotate: "0",
+                fill: function() { return "none"; },
+                stroke: function(d) {
+                  return vars.color(vars.accessor_items(d)[vars.var_color]); 
+                },
                 func: d3.svg.line()
                      .interpolate(vars.interpolate)
                      .x(function(d) { return vars.x_scale[0]["func"](d[vars.time.var_time]); })
-                     .y(function(d) { return vars.y_scale(d[vars.var_y]); })
+                     .y(function(d) { return vars.y_scale[0]["func"](d[vars.var_y]); })
               }]
           }]
 
         };
+
+        vars = vistk.utils.merge(vars, vars.params);
 
         // Events handlers
         vars.evt.register("highlightOn", function(d) { });
@@ -44,27 +62,24 @@
        // var parseDate = d3.time.format("%y-%b-%d").parse,
         var formatPercent = d3.format(".0%");
 
-        vars.x_scale = d3.time.scale()
-            .range([0, vars.width]);
-
         vars.y_scale = d3.scale.linear()
             .range([vars.height, 0])
             .domain(d3.extent(vars.data, function(d) { return d[vars.var_y]; }));
 
         vars.xAxis = d3.svg.axis()
-            .scale(vars.x_scale)
+            .scale(vars.x_scale[0]["func"])
             .orient("bottom");
 
-        var yAxis = d3.svg.axis()
+        vars.yAxis = d3.svg.axis()
             .scale(vars.y_scale)
             .orient("left")
             .tickFormat(formatPercent);
 
-        vars.x_scale.domain(d3.extent(vars.data, function(d) { return d[vars.time.var_time]; }));
+        vars.x_scale[0]["func"].domain(d3.extent(vars.data, function(d) { return d[vars.time.var_time]; }));
 
         vars.area = d3.svg.area()
             .interpolate('cardinal')
-            .x(function(d) { return vars.x_scale(d[vars.time.var_time]); })
+            .x(function(d) { return vars.x_scale[0]["func"](d[vars.time.var_time]); })
             .y0(function(d) { return vars.y_scale(d.y0); })
             .y1(function(d) { return vars.y_scale(d.y0 + d.y); });
 
@@ -96,7 +111,7 @@
 
         vars.svg.append("g")
             .attr("class", "y axis")
-            .call(yAxis);
+            .call(vars.yAxis);
 
       break;
       
