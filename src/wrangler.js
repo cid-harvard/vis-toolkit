cdb7a6039fab7a468ab013610d19d0d081c2e11c
romsson <romain.vuillemot@gmail.com>
2015-06-19 20:18:15 -0400

    Fixing data wrangling for nested charts

--- src/wrangler.js
+++ src/wrangler.js
@@ -76,6 +76,15 @@
           v[vars.time.var_time] = d[vars.time.var_time];
           v[vars.var_y] = d[vars.var_y];
           v[vars.var_x] = d[vars.var_x];
+
+          // Make sure we retrieve the data for nested charts
+          vars.items.forEach(function(item) {
+            item.marks.forEach(function(params) {
+              v[params.var_y] = d[params.var_y];
+              v[params.var_x] = d[params.var_x];
+            })
+          })
+
           return v;
         })
 
@@ -144,6 +153,7 @@
           aggregation.piescatter[1] = {};
 
           aggregation.piescatter[0][vars.var_share] = d3.sum(leaves, function(d) {
+
             if(vars.share_cutoff(d)) {
               return 1;
             } else {
@@ -152,6 +162,7 @@
           });
 
           aggregation.piescatter[1][vars.var_share] = d3.sum(leaves, function(d) {
+
             if(!vars.share_cutoff(d)) {
               return 1;
             } else {
@@ -160,6 +171,7 @@
           });
 
           vars.columns.forEach(function(c) {
+
             if(c === vars.var_text || c === vars.var_group) {
               return;
             }
@@ -177,54 +189,6 @@
       vars.new_data = nested_data.map(function(d) { return d.values; });
 
     }
-
-/*
-    // vars.time_data format
-    // {id:, name:, values: [{date: d[vars.time.var_time], rank:, year:]}
-    if(vars.type === "stacked") {
-
-      if(vars.time_data === null) {
-        // Parse time in raw data
-        vars.data.forEach(function(d) {
-          d[vars.time.var_time] = vars.time.parse(d[vars.time.var_time]);
-        });
-
-        // Retriev the unique items from the raw data
-        vars.unique_items = d3.set(vars.new_data.map(function(d) { return d[vars.var_text]; })).values();
-
-      }
-
-      // Find unique items and create ids
-      vars.time_data = vars.unique_items.map(function(c, i) {
-
-        var time_values = vars.data.filter(function(d) {
-          return d[vars.var_text] === c;
-        });
-
-        // Init with corresponding time slice
-        var m = vars.new_data[i];
-
-        // Create time values
-        var data_values = time_values.map(function (d) {
-            
-              var v = {}; 
-              v[vars.time.var_time] = d[vars.time.var_time];
-              v[vars.var_y] = d[vars.var_y];
-              v[vars.var_x] = d[vars.var_x];
-              v[vars.var_id] = d[vars.var_id];
-              return v;
-            
-          })
-
-        m.values = data_values;
-
-        return m;
-      });
-
-    }
-*/
-
-
 
     // Chart specific metadata: stacked
     if(vars.type === "stacked") {
@@ -264,42 +228,6 @@
 
        vars.new_data = vars.stack(vars.new_data);
     }
-      // Find the number or years
-
-/*
-      unique_years = d3.set(vars.data.data.map(function(d) { return d.year;})).values();
-
-      data = [];
-
-      unique_years.forEach(function(d) {
-
-        a = {};
-
-        vars.data.data.filter(function(e) {
-          return e.year == d;
-        })
-        .map(function(e) {
-          a[e.abbrv] = e.share;
-    //      return a;
-        })
-
-        a.date = vars.time.parse(d);
-        data = data.concat(a);
-
-      });
-
-      vars.new_data = stack(d3.keys(data[0]).filter(function(key) { return key !== "date"; }).map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {date: d.date, y: d[name]};
-          })
-        };
-      }));
-
-
-    }
-*/
 
     if(vars.links !== null) {
 
