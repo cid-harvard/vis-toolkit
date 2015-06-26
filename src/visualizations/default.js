c70a06fb6657e3c947ec30ff72af5850f9044e3e
romsson <romain.vuillemot@gmail.com>
2015-06-26 11:54:23 -0400

    Updated product space stepper with zoom

--- src/visualizations/default.js
+++ src/visualizations/default.js
@@ -145,8 +145,13 @@
           // ITEMS EXIT
           var gItems_exit = gItems.exit().remove();
 
-          if(vars.zoom.length > 0)
+          if(vars.zoom.length > 0) {
             vistk.utils.zoom_to_nodes(vars.zoom);
+          } else {
+            
+            vars.svg.transition()
+                    .attr("transform", "");
+          }
 
         }
         
