7166769cf7b67480a344ff5df91bf8d471f6063b
romsson <romain.vuillemot@gmail.com>
2015-06-29 09:50:49 -0400

    Updated parameters

--- src/visualizations/grid.js
+++ src/visualizations/grid.js
@@ -1,74 +1,38 @@
-        case "grid_old":
+vars.default_params["grid"] = function(scope) {
 
-          vars.params = {
+  var params = {};
 
-            x_scale: [{
-                name: "linear",
-                func: d3.scale.linear()
-                      .domain([0, nb_dimension])
-                      .range([0, vars.width])
-              }
-            ],
+  params.x_scale = [{
+    name: "linear",
+    func: d3.scale.linear()
+          .domain([0, nb_dimension])
+          .range([0, vars.width])
+  }];
 
-            y_scale: [{
-                name: "linear",
-                func: d3.scale.linear()
-                      .domain([0, nb_dimension])
-                      .range([0, vars.height])
-              }
-            ],
+  params.y_scale = [{
+    name: "linear",
+    func: d3.scale.linear()
+          .domain([0, nb_dimension])
+          .range([0, vars.height])
+  }];
 
-            items: [{
-              attr: "name",
-              marks: [{
-                type: "diamond",
-                rotate: "0"
-              },{
-                type: "text",
-                rotate: "-30"
-              }]
-            }],
-            var_x: "x",
-            var_y: "y",
-          };
+  params.items = [{
+    attr: "name",
+    marks: [{
+      type: "diamond",
+      rotate: "0"
+    }, {
+      type: "text",
+      rotate: "-30"
+    }]
+  }];
 
-          vars = vistk.utils.merge(vars, vars.params);
-          vars.items = vistk.utils.merge(vars.items, vars.user_vars.items);
+  params.var_x = "x";
+  params.var_y = "y";
 
-          // PRE-UPDATE
-          var gItems = vars.svg.selectAll(".mark__group")
-                           .data(vars.new_data, function(d, i) { return d[vars.var_id]; });
+  params.x_axis_show = false;
+  params.y_axis_show = false;
 
-          // ENTER
-          var gItems_enter = gItems.enter()
-                          .append("g")
-                          .each(vistk.utils.items_group)
-                          .attr("transform", function(d, i) {
-                            return "translate(" + vars.width / 2 + ", " + vars.height / 2 + ")";
-                          });
+  return params;
 
-          // Add graphical marks
-          vars.items[0].marks.forEach(function(params) {
-
-            // Enter mark
-            gItems_enter.call(vistk.utils.draw_mark, params);
-
-            // Update mark
-            gItems.call(vistk.utils.draw_mark, params);
-
-          });
-
-          // EXIT
-          var gItems_exit = gItems.exit().remove();
-
-          // POST-UPDATE
-          vars.svg.selectAll(".mark__group")
-                          .classed("highlighted", function(d, i) { return d.__highlighted; })
-                          .transition()
-                          .delay(function(d, i) { return (i / vars.new_data.length) * 100; })
-                          .duration(vars.duration)
-                          .attr("transform", function(d, i) {
-                            return "translate(" + vars.x_scale[0]["func"](d[vars.var_x]) + ", " + vars.y_scale[0]["func"](d[vars.var_y]) + ")";
-                          });
-
-          break;
+};
--- src/visualizations/stacked.js
+++ src/visualizations/stacked.js
@@ -1,91 +1,52 @@
-      case "stacked_old":
+vars.default_params["stacked"] = function(scope) {
 
-        vars.params = {
+  var params = {};
 
-          accessor_values: function(d) { return d.values; },
-          accessor_items: function(d) { return d; },
+  params.accessor_values = function(d) { return d.values; };
+  params.accessor_items = function(d) { return d; };
 
-          x_scale: [{
-              name: "linear",
-              func: d3.time.scale()
-                      .range([vars.margin.left, vars.width - vars.margin.left - vars.margin.right])
-                      .domain(vars.time.interval)
-            }
-          ],
+  params.x_scale = [{
+    name: "linear",
+    func: d3.time.scale()
+            .range([vars.margin.left, vars.width - vars.margin.left - vars.margin.right])
+            .domain(vars.time.interval)
+  }];
 
-          y_scale: [{
-              name: "linear",
-              func: d3.scale.linear()
-                      .range([vars.margin.top, vars.height - vars.margin.top - vars.margin.bottom])
-                      .domain([d3.max(vars.new_data, function(d) { return d[vars.var_y]; }), 0])
-            }
-          ],
+  params.y_scale = [{
+    name: "linear",
+    func: d3.scale.linear()
+            .range([vars.margin.top, vars.height - vars.margin.top - vars.margin.bottom])
+            .domain(d3.extent(Array.prototype.concat.apply([], vars.new_data.map(function(d) { return d.values; }) ), function(d) { return d[vars.var_y]; }))
+  }];
 
-          connect: [{
-            attr: vars.time.var_time,
-            marks: [{
-                type: "path",
-                rotate: "0",
-                fill: function(d) { return vars.color(d.name); },
-                stroke: function(d) {
-                  return vars.color(vars.accessor_items(d)[vars.var_color]); 
-                },
-                func: d3.svg.area()
-                        .interpolate('cardinal')
-                        .x(function(d) { return vars.x_scale[0]["func"](d[vars.time.var_time]); })
-                        .y0(function(d) { return vars.y_scale[0]["func"](d.y0); })
-                        .y1(function(d) { return vars.y_scale[0]["func"](d.y0 + d.y); })
-              }]
-          }]
+  params.connect = [{
+    attr: vars.time.var_time,
+    marks: [{
+        type: "path",
+        rotate: "0",
+        fill: function(d) { return vars.color(d[vars.var_color]); },
+        stroke: function(d) {
+          return vars.color( params.accessor_items(d)[vars.var_color]); 
+        },
+        func: d3.svg.area()
+                .interpolate('cardinal')
+                .x(function(d) { return vars.x_scale[0]["func"](d[vars.time.var_time]); })
+                .y0(function(d) { return vars.y_scale[0]["func"](d.y0); })
+                .y1(function(d) { return vars.y_scale[0]["func"](d.y0 + d.y); })
+      }]
+  }];
 
-        };
+  params.x_axis_show = true;
+  params.x_axis_translate = [0, vars.height - vars.margin.bottom - vars.margin.top];
+  params.x_grid_show = true;
+  params.x_ticks = vars.time.points.length;
+  params.x_format = d3.time.format("%Y");
+  params.x_text = false;
 
-        vars = vistk.utils.merge(vars, vars.params);
+  params.y_axis_show = true;
+  params.y_axis_translate = [vars.margin.left, 0];
+  params.y_grid_show = true;
 
-        vars.x_axis = d3.svg.axis()
-            .scale(vars.x_scale[0]["func"])
-            .orient("bottom(");
+  return params;
 
-        vars.y_axis = d3.svg.axis()
-            .scale(vars.y_scale[0]["func"])
-            .orient("left");
-
-        // Connect marks
-        var gConnect = vars.svg.selectAll(".connect__group")
-                        .data(vars.new_data, function(d, i) { return i; });
-      
-        var gConnect_enter = gConnect.enter()
-                        .append("g")
-                        .attr("class", "connect__group")
-                        .attr("id", function(d) { return d[vars.var_id]; })
-                        .style("opacity", 0.2);
-
-        vars.connect[0].marks.forEach(function(d) {
-          
-          vars.mark.type = d.type;
-          vars.mark.rotate = d.rotate;
-          vars.mark.fill = d.fill;
-          vars.mark.stroke = d.stroke;
- 
-          gConnect_enter.each(vistk.utils.connect_mark);
-
-        });
-
-        vars.svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + (vars.height - vars.margin.bottom) + ")")
-            .call(vars.x_axis);
-             
-        vars.svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + (vars.margin.left) + ",0)")
-            .call(vars.y_axis)
-          .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "-1.71em")
-            .style("text-anchor", "end")
-            .text(vars.y_text);
-
-      break;
-      
+};
