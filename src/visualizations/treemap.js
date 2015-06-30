17ba118495217a64ff76c36b1be6e44e52237853
romsson <romain.vuillemot@gmail.com>
2015-06-30 14:54:23 -0400

    Towards configuration for treemaps

--- src/visualizations/treemap.js
+++ src/visualizations/treemap.js
@@ -1,34 +1,41 @@
       case "treemap":
 
-        vars.params = {
-          x_scale: [{
-              name: "linear",
-              func: d3.scale.linear()
-                      .range([vars.margin.left, vars.width-vars.margin.left-vars.margin.right])
-                      .domain(vars.new_data.map(function(d) { return d.x; })),
-            }
-          ],
+        var treemap_params = function(scope) {
 
-          y_scale: [{
+          var params = {};
+
+          params.x_scale = [{
+            name: "linear",
+            func: d3.scale.linear()
+                    .range([vars.margin.left, vars.width-vars.margin.left-vars.margin.right])
+                    .domain(vars.new_data.map(function(d) { return d.x; })),
+          }];
+
+          params.y_scale = [{
             name: "linear",
             func: d3.scale.linear()
                     .range([vars.height-vars.margin.top-vars.margin.bottom, vars.margin.top])
                     .domain([0, d3.max(vars.new_data, function(d) { return d.y; })]),
-          }],
+          }];
 
-          items: [{
-            attr: "depth_2",
+          params.items = [{
             marks: [{
               type: "rect",
+              rotate: "0",
+            }, {
+              type: "circle",
               rotate: "0"
             }]
-          }],
+          }];
 
-          x_ticks: 10,
+          params.x_ticks = 10;
 
+          return params;
         };
 
-        vars = vistk.utils.merge(vars, vars.params);
+        t_params = treemap_params(vars);
+
+        vars = vistk.utils.merge(vars, t_params);
 
           // LOAD USER PARAMS
         vars.items = vistk.utils.merge(vars.items, vars.user_vars.items);
@@ -58,43 +65,39 @@
                       });
 
         // Add graphical marks
-        vars.items[0].marks.forEach(function(d) {
+        t_params.items[0].marks.forEach(function(params) {
 
-          vars.mark.type = d.type;
-          vars.mark.rotate = d.rotate;
-          vars.mark.translate = d.translate;
+        gItems_enter
+              .filter(function(d, j) {
+                  return (params.type == "rect" && d.depth == 2) || (params.type == "text" && d.depth == 1);
+                })
+              .call(utils.draw_mark, params);
 
-          gItems_enter
-                .filter(function(d, j) {
-                    return (vars.mark.type == "rect" && d.depth == 2) || (vars.mark.type == "text" && d.depth == 1);
-                  })
-                .each(utils.items_mark)
+        gItems.select("rect")
+              .transition().duration(vars.duration)
+              .attr("x", 0)
+              .attr("y", 0)
+              .attr("width", function(d) { return d.dx; })
+              .attr("height", function(d) { return d.dy; });
 
-          gItems.select("rect")
-                .transition().duration(vars.duration)
-                .attr("x", 0)
-                .attr("y", 0)
-                .attr("width", function(d) { return d.dx; })
-                .attr("height", function(d) { return d.dy; });
-
-          // Only label the parents
-          gItems_enter
-                 .filter(function(d, j) {
-                   return d.depth == 1 &&  d.dx > 30 && d.dy > 30;
-                 })
-                 .append("foreignObject")
-                 .attr("width", function(d) { return (d.dx - vars.padding) + "px"; })
-                 .attr("height", function(d) { return (d.dy - 2*vars.padding) + "px"; })
-               .append("xhtml:body")
-                 .style("font", "14px 'Helvetica Neue'")
-               .append("div")
-                 .style("padding-top", function(d) { return (vars.padding/2)+"px"; })
-                 .style("width", function(d) { return (d.dx - 2*vars.padding) + "px"; })
-                 .style("height", function(d) { return (d.dy - 2*vars.padding) + "px"; })
-                 .style({"text-overflow": "ellipsis", "overflow": "hidden"})
-                 .html(function(d) {
-                   return vars.accessor_data(d)[vars.var_text];
-                 })
+        // Only label the parents
+        gItems_enter
+               .filter(function(d, j) {
+                 return d.depth == 1 &&  d.dx > 30 && d.dy > 30;
+               })
+               .append("foreignObject")
+               .attr("width", function(d) { return (d.dx - vars.padding) + "px"; })
+               .attr("height", function(d) { return (d.dy - 2*vars.padding) + "px"; })
+             .append("xhtml:body")
+               .style("font", "14px 'Helvetica Neue'")
+             .append("div")
+               .style("padding-top", function(d) { return (vars.padding/2)+"px"; })
+               .style("width", function(d) { return (d.dx - 2*vars.padding) + "px"; })
+               .style("height", function(d) { return (d.dy - 2*vars.padding) + "px"; })
+               .style({"text-overflow": "ellipsis", "overflow": "hidden"})
+               .html(function(d) {
+                 return vars.accessor_data(d)[vars.var_text];
+               })
 
           // SVG wrap
           // new_items.selectAll("text").call(vistk.utils.wrap);
