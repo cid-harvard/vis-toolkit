8a85671e83b0f2c2cfc9080a43ca4037611d29a6
romsson <romain.vuillemot@gmail.com>
2015-07-06 15:37:58 -0400

    Init tests using Jasmine

---
+++ test/specs/HelloWorldSpec.js
@@ -0,0 +1,11 @@
+describe("Hello world my version is", function() {
+  it("says hello and the current version", function() {
+    expect(vistk.version).toEqual("0.2");
+  });
+});
+
+describe('JavaScript addition operator', function () {
+    it('adds two numbers together', function () {
+        expect(1 + 2).toEqual(3);
+    });
+});
