011afa9e92af17312d39dade488ebe2d56cd70da
romsson <romain.vuillemot@gmail.com>
2015-07-15 09:35:19 -0400

    More cleanup

--- src/visualizations/geomap.js
+++ src/visualizations/geomap.js
@@ -2,33 +2,33 @@
 
   var params = {};
 
+  params.accessor_data = function(d) { return d.data; };
+  params.accessor_values = function(d) { return d.data.values; };
+
   params.x_scale = [{
-    name: "linear",
     func: d3.scale.linear()
             .domain(d3.extent(vars.countries, function(d) { return d[vars.var_x]; }))
             .range([scope.margin.left, scope.width - scope.margin.left - scope.margin.right])
   }];
 
   params.y_scale = [{
-    name: "linear",
     func: d3.scale.linear()
             .domain(d3.extent(vars.countries, function(d) { return d[vars.var_y]; }))
             .range([scope.height - vars.margin.top - vars.margin.bottom, scope.margin.top])
   }];
 
   params.items = [{
-    attr: "country",
     marks: [{
       type: "shape",
       fill: d3.scale.linear()
-              .domain([d3.min(vars.new_data, function(d) { return d[vars.var_color]; }), d3.max(vars.new_data, function(d) { return d[vars.var_color]; })])
-              .range(["red", "green"]),
-      rotate: 0
+              .domain([d3.min(vars.new_data, function(d) { 
+                  return d[vars.var_color]; 
+                }), d3.max(vars.new_data, function(d) { 
+                  return d[vars.var_color]; 
+                })])
+              .range(["red", "green"])
     }]
   }];
-
-  params.accessor_data = function(d) { return d.data; };
-  params.accessor_values = function(d) { return d.data.values; };
 
   return params;
 
--- src/visualizations/treemap.js
+++ src/visualizations/treemap.js
@@ -3,21 +3,18 @@ vars.default_params["treemap"] = function(scope) {
   var params = {};
 
   params.x_scale = [{
-    name: "linear",
     func: d3.scale.linear()
             .range([scope.margin.left, scope.width - scope.margin.left - scope.margin.right])
             .domain([0, d3.max(vars.new_data, function(d) { return d.x; })]),
   }];
 
   params.y_scale = [{
-    name: "linear",
     func: d3.scale.linear()
             .range([scope.margin.top, scope.height - scope.margin.top - scope.margin.bottom])
             .domain([0, d3.max(vars.new_data, function(d) { return d.y; })]),
   }];
 
   params.items = [{
-    attr: "bar",
     marks: [{
       type: "divtext",
       filter: function(d, i) { return d.depth == 1 && d.dx > 30 && d.dy > 30; }
