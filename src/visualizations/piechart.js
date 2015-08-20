5566a7e46ff66ebb6630d3058a4420c716aaa305
romsson <romain.vuillemot@gmail.com>
2015-08-20 11:40:26 -0400

    Updated config

--- src/visualizations/piechart.js
+++ src/visualizations/piechart.js
@@ -17,12 +17,12 @@ vars.default_params["piechart"] = function(scope) {
 
   params.r_scale = d3.scale.linear()
               .range([0, scope.width/6])
-              .domain([0, d3.max(vars.new_data, function(d) { return vars.accessor_data(d)[vars.var_share]; })]);
+              .domain([0, d3.max(scope.new_data, function(d) { return scope.accessor_data(d)[scope.var_share]; })]);
 
   params.items = [{
     marks: [{
       type: "arc",
-      fill: function(d) { return vars.color(vars.accessor_items(d)[vars.var_color]); }
+      fill: function(d) { return scope.color(scope.accessor_items(d)[scope.var_color]); }
     }]
   }];
 
@@ -31,6 +31,10 @@ vars.default_params["piechart"] = function(scope) {
   
   params.y_axis_show = false;
   params.y_grid_show = false;
+
+  params.postprocessing = function() {
+
+  }
 
   return params;
 
