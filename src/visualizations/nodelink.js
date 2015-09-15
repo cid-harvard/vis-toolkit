a3c234cb22812c1537c4e55ced2da2612b96c464
romsson <romain.vuillemot@gmail.com>
2015-09-15 11:20:08 -0400

    Updated config

---
+++ src/visualizations/nodelink.js
@@ -0,0 +1,54 @@
+vars.default_params["productspace"] = function(scope) {
+
+  var params = {};
+
+  params.x_scale = [{
+    func: d3.scale.linear()
+            .range([scope.margin.left, vars.width-vars.margin.left-vars.margin.right])
+            .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_x]; })).nice()
+  }];
+
+  params.y_scale = [{
+    func: d3.scale.linear()
+            .range([vars.height-vars.margin.top-vars.margin.bottom, vars.margin.top])
+            .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_y]; })).nice(),
+  }];
+
+  params.r_scale = d3.scale.linear()
+              .range([10, 30])
+              .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_r]; }));
+
+  params.items = [{
+    marks: [{
+      type: "circle",
+      r_scale: d3.scale.linear()
+                  .range([10, 30])
+                  .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_r]; })),
+      fill: function(d) { return vars.color(vars.accessor_items(d)[vars.var_color]); }
+    }, {
+      type: "text",
+      rotate: "30",
+      translate: null
+    }]
+  }];
+
+  params.connect = [{
+    attr: "links",
+    type: "items",
+    marks: [{
+      type: "line",
+      rotate: "0",
+      func: null,
+    }]
+  }];
+
+  params.x_axis_show = false;
+  params.x_grid_show = false;
+
+  params.y_axis_show = false;
+  params.y_grid_show = false;
+
+  return params;
+
+};
+
