0c09938843cc4a23bd34f869728c4dee92088247
romsson <romain.vuillemot@gmail.com>
2015-10-14 15:42:37 -0400

    Click on background reset the selections

--- src/star_constructor.js
+++ src/star_constructor.js
@@ -15,8 +15,31 @@
           .attr("height", vars.height)
           .style('overflow', 'visible')
           .style('z-index', 0)
+          .on("click", function(d) {
+
+            if(vars.type === "productspace") {
+
+              vars.links.forEach(function(e) {
+                e.__selected = false;
+                e.__redraw = true;
+              });
+
+              vars.new_data.forEach(function(f, k) {
+                f.__selected = false;
+                f.__redraw = true;
+              });
+
+              vars.zoom = [];
+              vars.init = true;
+              vars.refresh = true;
+              console.log("REFH", vars, vars.init, vars.refresh)
+              d3.select(vars.container).call(vars.this_chart);
+            }
+
+          })
         .append("g")
-          .attr("transform", "translate(" + vars.margin.left + "," + vars.margin.top + ")rotate(" + vars.rotate + ")");
+          .attr("transform", "translate(" + vars.margin.left + "," + vars.margin.top + ")rotate(" + vars.rotate + ")")
+
 
       } else {
         // HTML Container for table
