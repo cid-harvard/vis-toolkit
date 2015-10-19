bbe58c57619a2f3d724330a6215a4249126c1449
romsson <romain.vuillemot@gmail.com>
2015-10-19 15:51:33 -0400

    More progress on zoom

--- src/end_constructor.js
+++ src/end_constructor.js
@@ -1,103 +1,102 @@
 
-setTimeout(function() {
+  setTimeout(function() {
 
-  vars.evt.register("highlightOn", function(d) {
+    vars.evt.register("highlightOn", function(d) {
 
-    // POST-RENDERING STUFF
-    // Usually aimed at updating the rendering order of elements
-    vars.z_index.forEach(function(d) {
+      // POST-RENDERING STUFF
+      // Usually aimed at updating the rendering order of elements
+      vars.z_index.forEach(function(d) {
 
-      // Filter events
-      if(vars.type === d.type && d.event === 'highlightOn') {
+        // Filter events
+        if(vars.type === d.type && d.event === 'highlightOn') {
 
-        vars.svg.selectAll(d.selector)
-          .filter(function(e) {
-            if(typeof d.attribute !== 'undefined') {
-              return e[d.attribute];
-            } else {
-              return true;
-            }
-          })
-          .each(function() {
-            this.parentNode.appendChild(this);
-          });
-      }
+          vars.svg.selectAll(d.selector)
+            .filter(function(e) {
+              if(typeof d.attribute !== 'undefined') {
+                return e[d.attribute];
+              } else {
+                return true;
+              }
+            })
+            .each(function() {
+              this.parentNode.appendChild(this);
+            });
+        }
 
-    });
+      });
 
-    d3.select(vars.container).call(vars.this_chart);
-
-  });
-
-  vars.evt.register("highlightOut", function(d) {
-
-    if(vars.type !== "productspace") {
       d3.select(vars.container).call(vars.this_chart);
-    }
-
-    vars.z_index.forEach(function(d) {
-
-      // Filter events
-      if(vars.type === d.type && d.event === 'highlightOut') {
-
-        vars.svg.selectAll(d.selector)
-          .filter(function(e) {
-            if(typeof d.attribute !== 'undefined') {
-              return e[d.attribute];
-            } else {
-              return true;
-            }
-          })
-          .each(function() {
-            this.parentNode.appendChild(this);
-          });
-      }
 
     });
 
-	});
+    vars.evt.register("highlightOut", function(d) {
 
-  vars.root_svg.on("click", function(d) {
+      if(vars.type !== "productspace") {
+        d3.select(vars.container).call(vars.this_chart);
+      }
 
-    if(vars.type === "productspace") {
+      vars.z_index.forEach(function(d) {
 
-      vars.links.forEach(function(e) {
-        e.__selected = false;
-        e.__redraw = true;
+        // Filter events
+        if(vars.type === d.type && d.event === 'highlightOut') {
+
+          vars.svg.selectAll(d.selector)
+            .filter(function(e) {
+              if(typeof d.attribute !== 'undefined') {
+                return e[d.attribute];
+              } else {
+                return true;
+              }
+            })
+            .each(function() {
+              this.parentNode.appendChild(this);
+            });
+        }
+
       });
 
-      vars.new_data.forEach(function(f, k) {
-        f.__selected = false;
-        f.__redraw = true;
-      });
+    });
 
-      vars.zoom = [];
+    vars.root_svg.on("click", function(d) {
 
-      vars.svg.transition()
-              .duration(vars.duration)
-              .attr("transform", "translate(" + vars.margin.left + "," + vars.margin.top + ")rotate(" + vars.rotate + ")");
+      if(vars.type === "productspace") {
 
-      vars.init = true;
-      vars.refresh = true;
+        vars.links.forEach(function(e) {
+          e.__selected = false;
+          e.__redraw = true;
+        });
 
+        vars.new_data.forEach(function(f, k) {
+          f.__selected = false;
+          f.__redraw = true;
+        });
 
-      d3.select(vars.container).selectAll(".connect__line")
-        .classed("highlighted", function(d, i) { return false; })
-        .classed("highlighted__adjacent", function(d, i) { return false; })
-        .classed("selected", function(d, i) { return false; })
-        .classed("selected__adjacent", function(d, i) { return false; })
+        vars.zoom = [];
+        vars.selection = [];
+        vars.highlight = [];
 
-      d3.select(vars.container).selectAll("circle")
-        .classed("highlighted", function(d, i) { return false; })
-        .classed("highlighted__adjacent", function(d, i) { return false; })
-        .classed("selected", function(d, i) { return false; })
-        .classed("selected__adjacent", function(d, i) { return false; })
+        utils.zoom_to_nodes(vars.zoom);
 
-    }
+        vars.init = true;
+        vars.refresh = true;
 
-  })
+        d3.select(vars.container).selectAll(".connect__line")
+          .classed("highlighted", function(d, i) { return false; })
+          .classed("highlighted__adjacent", function(d, i) { return false; })
+          .classed("selected", function(d, i) { return false; })
+          .classed("selected__adjacent", function(d, i) { return false; });
 
-}, 100)
+        d3.select(vars.container).selectAll("circle")
+          .classed("highlighted", function(d, i) { return false; })
+          .classed("highlighted__adjacent", function(d, i) { return false; })
+          .classed("selected", function(d, i) { return false; })
+          .classed("selected__adjacent", function(d, i) { return false; });
+
+      }
+
+    })
+
+  }, 100)
 
   vars.this_chart = chart;
 
