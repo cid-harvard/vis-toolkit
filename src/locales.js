940ddf9fc367ed19a95576bea4271a9c494f04e6
romsson <romain.vuillemot@gmail.com>
2015-11-16 10:02:16 -0500

    Translations in separate file

---
+++ src/locales.js
@@ -0,0 +1,42 @@
+vars.locales = {
+  'en_US': {
+    'complexity': 'Complexity',
+    'low': 'Low',
+    'high': 'High',
+    'export': 'Export',
+    'non-export': 'Non-Export',
+    'similarity_link': 'Similarity Link',
+    'loading': 'Loading...',
+    'no-data': 'No data is available'
+  },
+  'en_EN': {
+    'complexity': 'Complexity',
+    'low': 'Low',
+    'high': 'High',
+    'export': 'Export',
+    'non-export': 'Non-Export',
+    'similarity_link': 'Similarity Link',
+    'loading': 'Loading...',
+    'no-data': 'No data is available'
+  },
+  'es_ES': {
+    'complexity': 'Complejidad',
+    'low': 'Bajo',
+    'high': 'Alto',
+    'export': 'Exportación',
+    'non-export': 'No-Exportación',
+    'similarity_link': 'Enlar de similitud',
+    'loading': 'Cargando...',
+    'no-data': 'Sin datos disponibles'
+  },
+  'fr_FR': {
+    'complexity': 'Complexité',
+    'low': 'Basse',
+    'high': 'Haute',
+    'export': 'Export',
+    'non-export': 'Non-Export',
+    'similarity_link': 'Lien de similarité',
+    'loading': 'Chargement...',
+    'no-data': 'Aucune donnée disponible'
+  }
+},
