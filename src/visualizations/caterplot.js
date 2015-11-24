c217fcae35fd4ec598c44a9579a90eb0ecf620a0
romsson <romain.vuillemot@gmail.com>
2015-11-24 10:46:22 -0500

    Improving caterplots

--- src/visualizations/caterplot.js
+++ src/visualizations/caterplot.js
@@ -22,15 +22,15 @@ vars.default_params["caterplot"] = function(scope) {
               .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_r]; }));
 
   params.items = [{
-    translate: function() {
-      return [params.x_scale[0]['func'].rangeBand() / 2, 0]
-    },
     marks: [{
         type: 'circle',
         r_scale: d3.scale.linear()
                     .range([vars.radius_min, vars.radius_max])
                     .domain(d3.extent(vars.new_data, function(d) { return d[vars.var_r]; })),
         fill: function(d) { return vars.color(vars.accessor_items(d)[vars.var_color]); },
+        translate: function() {
+          return [vars.x_scale[0]['func'].rangeBand() / 4, 0]
+        },
       }, {
       var_mark: '__highlighted',
       type: d3.scale.ordinal().domain([true, false]).range(['text', 'none'])
@@ -47,6 +47,7 @@ vars.default_params["caterplot"] = function(scope) {
   params.y_axis_show = true;
   params.y_axis_translate = [scope.margin.left, 0];
   params.y_grid_show = true;
+  params.y_invert = false;
 
   return params;
 
