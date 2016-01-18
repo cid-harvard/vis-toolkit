9017bd6a336ee6ab070b955ba8b9043cd72580e1
romsson <romain.vuillemot@gmail.com>
2016-01-18 15:35:24 +0100

    Adding more tests

--- test/index-test.js
+++ test/index-test.js
@@ -3,14 +3,14 @@ var test = require('tape');
 d3 = require("d3");
 vistk = require("../build/vistk.js");
 
-test('VisTK metadata checks', function (t) {
+test('metadata checks', function (t) {
 
     t.plan(1);
     t.equal(typeof vistk.version, 'string');
 
 });
 
-test('An empty configuration object returns an object with default values', function (t) {
+test('an empty configuration object returns an object with default values', function (t) {
 
     t.plan(1);
     var visualization = vistk.viz().params({});
@@ -18,7 +18,16 @@ test('An empty configuration object returns an object with default values', func
 
 });
 
-test('A dataset with no attribute is augmented with attributes', function (t) {
+test('the same dataset is returned by VisTK', function (t) {
+
+    t.plan(1);
+    var data = ['A', 'B', 'C'];
+    var visualization = vistk.viz().params({data: data});
+    t.equal(visualization.params().new_data, data);
+
+});
+
+test('a dataset with no attribute is augmented with attributes', function (t) {
 
     t.plan(1);
     var data = ['A', 'B', 'C'];
@@ -26,5 +35,17 @@ test('A dataset with no attribute is augmented with attributes', function (t) {
 
     // Default ids
     t.equal(visualization.params().new_data[0].__id, 0);
+
+});
+
+test('if the dataset is temporal, then only items are returned', function (t) {
+
+    t.plan(2);
+    var data = [{id: 1, time: 0}, {id: 1, time: 1}];
+    var visualization = vistk.viz().params({data: data});
+
+    // Default ids
+    t.equal(visualization.params().new_data[0].length, 1);
+    t.equal(visualization.params().new_data[0].values.length, 2);
 
 });
