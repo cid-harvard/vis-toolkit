ebeab72370619918c7ca099eab8845252a75704b
romsson <romain.vuillemot@gmail.com>
2016-01-26 15:44:38 +0100

    Testing axis values display

--- test/scatterplot-test.js
+++ test/scatterplot-test.js
@@ -2,7 +2,7 @@ var jsdom = require("jsdom");
 var test = require('tape');
 
 jsdom.env({
-  html:'<html><body><div id="viz"></div></body></html>',
+  html:'<html><body><div id="viz"></div><div id="viz_2"></div></body></html>',
   features: { QuerySelector:true }, //you need query selector for D3 to work
   scripts: ["https://cid-harvard.github.io/vis-toolkit/js/d3.js",
     "https://cid-harvard.github.io/vis-toolkit/build/vistk.js"],
@@ -49,7 +49,7 @@ jsdom.env({
       test('we should have 120 circles', function (t) {
 
         t.plan(1);
-        t.equal(d3.selectAll('circle')[0].length, 120);
+        t.equal(d3.select("#viz").selectAll('circle')[0].length, 120);
 
       });
 
@@ -60,7 +60,7 @@ jsdom.env({
 
         t.plan(2);
         t.equal(visualization.params().time.current_time, 2012);
-        t.equal(d3.selectAll('circle')[0].length, 120);
+        t.equal(d3.select("#viz").selectAll('circle')[0].length, 120);
 
       });
 
@@ -71,7 +71,7 @@ jsdom.env({
 
         t.plan(2);
         t.deepEqual(visualization.params().filter, ['Oceania']);
-        t.equal(d3.selectAll('circle')[0].length, 3);
+        t.equal(d3.select("#viz").selectAll('circle')[0].length, 3);
 
       });
 
@@ -83,7 +83,7 @@ jsdom.env({
 
         t.plan(2);
         t.deepEqual(visualization.params().filter, []);
-        t.equal(d3.selectAll('circle')[0].length, 120);
+        t.equal(d3.select("#viz").selectAll('circle')[0].length, 120);
 
       });
 
@@ -96,8 +96,78 @@ jsdom.env({
 
         t.plan(2);
         t.deepEqual(visualization.params().aggregate, visualization.params().var_group);
-        t.equal(d3.selectAll('circle')[0].length, 5);
+        t.equal(d3.select("#viz").selectAll('circle')[0].length, 5);
 
+      });
+
+      var flat_years = vistk.utils.flatten_years(countries);
+
+      var visualization_2 = vistk.viz()
+        .params({
+          dev: true,
+          type: 'scatterplot',
+          margin: {top: 10, right: 10, bottom: 30, left: 30},
+          container: '#viz_2',
+          data: flat_years,
+          var_id: 'name',
+          var_group: 'continent',
+          var_color: 'continent',
+          var_x: 'nb_products',
+          var_y: 'avg_products',
+          var_r: 'nb_products',
+          duration: 0,
+          time: {
+            var_time: "year",
+            current_time: "1995",
+            parse: function(d) { return d; }
+          },
+          x_scale: [{
+            domain: [0, 700]
+          }],
+          items: [{
+              marks: [{
+                var_mark: '__highlighted',
+                type: d3.scale.ordinal().domain([true, false]).range(["line_horizontal", "none"]),
+                offset_right: function(d, i, vars) {
+                    return vars.x_scale[0]["func"].range()[1] - vars.x_scale[0]["func"](d[vars.var_x]) + vars.r_scale(d[vars.var_r]);
+                }
+              }, {
+                var_mark: '__highlighted',
+                type: d3.scale.ordinal().domain([true, false]).range(["line_vertical", "none"]),
+                offset_top: function(d, i, vars) {
+                    return vars.r_scale(d[vars.var_r]);
+                }
+              }, {
+              type: 'circle'
+            }]
+          }],
+          highlight: ['France']
+        });
+
+      d3.select("#viz_2").call(visualization_2);
+
+      test('if highlight axis line values has been set', function (t) {
+
+        t.plan(5);
+        t.deepEqual(visualization_2.params().highlight, ['France']);
+        t.equal(d3.select('#viz_2').selectAll('.mark__line_horizontal')[0].length, 2);
+        t.equal(d3.select('#viz_2').selectAll('.mark__line_vertical')[0].length, 2);
+        t.equal(d3.select('#viz_2').selectAll('.items__mark__text.highlighted')[0].length, 2);
+        t.equal(d3.select('#viz_2').selectAll('.items__mark__rect.highlighted')[0].length, 2);
+      });
+
+      test('if highlight axis line values has been unset', function (t) {
+
+        visualization_2.params().highlight = [];
+        visualization_2.params().refresh = true;
+        d3.select("#viz_2").call(visualization_2);
+
+        t.plan(5);
+        t.deepEqual(visualization_2.params().highlight, []);
+        t.equal(d3.select('#viz_2').selectAll('.mark__line_horizontal')[0].length, 0);
+        t.equal(d3.select('#viz_2').selectAll('.mark__line_vertical')[0].length, 0);
+        t.equal(d3.select('#viz_2').selectAll('.items__mark__text.highlighted')[0].length, 0);
+        t.equal(d3.select('#viz_2').selectAll('.items__mark__rect.highlighted')[0].length, 0);
       });
 
     });
