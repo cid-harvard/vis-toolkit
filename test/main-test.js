c2e36fd12c70649927513032320b651c82217b0c
romsson <romain.vuillemot@gmail.com>
2016-02-04 14:46:22 +0100

    Re-organizing tests

--- test/main-test.js
+++ test/main-test.js
@@ -74,62 +74,6 @@ jsdom.env({
 
     });
 
-    test('a dataset of same size is returned by VisTK', function (t) {
-
-        t.plan(1);
-        var data = ['A', 'B', 'C'];
-        var visualization = vistk.viz().params({data: data});
-        d3.select("#viz").call(visualization);
-
-        t.equal(visualization.params().new_data.length, data.length);
-
-    });
-
-    test('a dataset with no attribute is augmented with attributes', function (t) {
-
-        t.plan(1);
-        var data = ['A', 'B', 'C'];
-        var visualization = vistk.viz().params({data: data});
-        d3.select("#viz").call(visualization);
-
-        // Default ids
-        t.equal(visualization.params().new_data[0].__id, 0);
-
-    });
-
-    test('if the dataset is temporal, then 1 item is created', function (t) {
-
-        t.plan(1);
-        var data = [{id: 1, time: 0}, {id: 1, time: 1}];
-        var visualization = vistk.viz().params({
-            data: data,
-            var_id: 'id',
-        });
-        d3.select("#viz").call(visualization);
-
-        t.equal(visualization.params().new_data.length, 1);
-
-    });
-
-    test('if the dataset is temporal, then item has 2 time values', function (t) {
-
-        t.plan(1);
-        var data = [{id: 1, time: 0, value: 0}, {id: 1, time: 1, value: 1}];
-        var visualization = vistk.viz().params({
-            data: data,
-            var_id: 'id',
-            time: {
-              var_time: 'time',
-              current_time: 0
-            },
-        });
-        d3.select("#viz").call(visualization);
-
-        t.equal(visualization.params().new_data[0].values.length, 2);
-
-    });
-
-
     test('check if selection and highlight classes are set', function (t) {
 
         t.plan(2);
