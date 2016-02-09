a6df1cb7dd472f428abc2eee94d124f29435079d
romsson <romain.vuillemot@gmail.com>
2016-02-09 16:51:08 +0100

    More tests on aggregations

--- test/aggregation-test.js
+++ test/aggregation-test.js
@@ -8,51 +8,73 @@ jsdom.env({
   features: { QuerySelector:true },
   done: function (err, window) {
 
-    var d3 = window.d3;
-    var vistk = window.vistk;
+  var d3 = window.d3;
+  var vistk = window.vistk;
 
-    test('if the dataset not aggregated', function (t) {
+  test('if the dataset not aggregated', function (t) {
 
-        t.plan(1);
-        var data = [{id: 0, group: 'A', value: 1},
-                    {id: 1, group: 'B', value: 1},
-                    {id: 2, group: 'A', value: 1},
-                    {id: 3, group: 'A', value: 1},
-                    {id: 4, group: 'A', value: 1},
-                    {id: 5, group: 'B', value: 1}];
+    t.plan(5);
+    var data = [{id: 0, group: 'A', value: 1},
+                {id: 1, group: 'B', value: 1},
+                {id: 2, group: 'A', value: 1},
+                {id: 3, group: 'A', value: 1},
+                {id: 4, group: 'A', value: 1},
+                {id: 5, group: 'B', value: 1}];
 
-        var v = vistk.viz()
-            .params({
-              type: 'piechart',
-              width: 600,
-              height: 400,
-              margin: {top: 10, right: 10, bottom: 30, left: 30},
-              data: data,
-              var_id: 'id',
-              container: '#viz',
-              var_color: 'id',
-              var_group: 'group',
-              var_x: function(d, i, vars) { return vars.width/2; },
-              var_y: function(d, i, vars) { return vars.height/2; },
-              var_share: 'value',
-              duration: 0,
-              items: [{
-                marks: [{
-                  var_mark: '__aggregated',
-                  type: d3.scale.ordinal().domain([true, false]).range(['arc', 'none']),
-                  fill: function(d, i, vars) {
-                    return vars.color(vars.accessor_data(d)[vars.var_color]);
-                  }
-                }]
-              }],
-            });
+    var v = vistk.viz()
+        .params({
+          type: 'piechart',
+          width: 600,
+          height: 400,
+          margin: {top: 10, right: 10, bottom: 30, left: 30},
+          data: data,
+          var_id: 'id',
+          container: '#viz',
+          var_color: 'id',
+          var_group: 'group',
+          var_x: function(d, i, vars) { return vars.width/2; },
+          var_y: function(d, i, vars) { return vars.height/2; },
+          var_share: 'value',
+          duration: 0,
+          items: [{
+            marks: [{
+              var_mark: '__aggregated',
+              type: d3.scale.ordinal().domain([true, false]).range(['arc', 'none']),
+              fill: function(d, i, vars) {
+                return vars.color(vars.accessor_data(d)[vars.var_color]);
+              }
+            }]
+          }],
+        });
 
-        d3.select("#viz").call(v);
+    d3.select("#viz").call(v);
+    t.equal(v.params().new_data.length, data.length);
 
-        t.equal(v.params().new_data.length, data.length);
-
+    v.params({
+      set: {
+        __aggregated: true
+      }
     });
 
+    d3.select("#viz").call(v);
+
+    // Number of aggregated items (number of different groups)
+    t.equal(v.params().new_data.length, 2);
+
+    // Aggregated values of items
+    t.equal(v.params().new_data[0].value, 4);
+    t.equal(v.params().new_data[1].value, 2);
+
+    v.params({
+      set: {
+        __aggregated: false
+      }
+    });
+
+    d3.select("#viz").call(v);
+    t.equal(v.params().new_data.length, data.length);
+
+  });
 
   }
 });
