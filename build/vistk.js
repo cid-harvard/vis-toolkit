ed185f3631f17323868787f286e195e2cf6d06d6
romsson <romain.vuillemot@gmail.com>
2016-02-22 11:07:12 +0100

    Build with string replace

--- build/vistk.js
+++ build/vistk.js
@@ -6,7 +6,7 @@ var w = typeof window === "undefined" ? this : window;
 var vistk = w.vistk || {};
 w.vistk = vistk;
 
-vistk.version = "{{ VERSION }}";
+vistk.version = "0.0.31";
 vistk.utils = {};
 
 if(typeof module === "object" && module.exports) {
